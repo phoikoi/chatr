1dfd873cb1b87e579cd6256e24846c178eac1bc3
Peter Hull <design.services@oikoi.com>
2017-04-07 23:32:07 -0700

    Rearranged preinstall.sh

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,12 +1,22 @@
+#!/bin/sh
+
+# Some things are commented out below because they're handy to have
+# but not essential for the chatr project.  In particular, if you
+# are using this project, you probably already have git installed. ;)
+
 apt-get install \
-    wget curl git htop vim-nox avahi-daemon tmux \
+    wget curl \
     build-essential llvm \
     postgresql libpq-dev \
-    redis-server nginx supervisor \
+    redis-server nginx-full supervisor \
     libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev \
     libpng-dev libjpeg-dev libtiff-dev libwebp-dev libfreetype6-dev \
     libxml2-dev libxslt1-dev \
     libevent-dev
+
+# apt-get install \
+#    git htop vim-nox avahi-daemon tmux module-assistant
+
 cat >>~/.bash_profile <<EOD
 export PATH="\$HOME/.pyenv/bin:\$PATH"
 eval "\$(pyenv init -)"
