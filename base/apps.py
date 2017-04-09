08863910dd0427b9be960f46ec7b10fd2d209efc
Peter Hull <design.services@oikoi.com>
2017-04-09 16:58:23 -0700

    Moved import into ready method

--- base/apps.py
+++ base/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 from base.signals import TickTock
-from base.models import run_clock_listeners
 
 class BaseConfig(AppConfig):
     name = 'base'
 
     def ready(self):
+        from base.models import run_clock_listeners
         TickTock.connect(run_clock_listeners)
