68e290bafd98fcc1322f7351775ee775144d3f74
Peter Hull <design.services@oikoi.com>
2017-04-09 17:49:12 -0700

    Always run at zero seconds (or close to it)

--- base/management/commands/ticktock.py
+++ base/management/commands/ticktock.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.timezone import now
 from channels import Channel
 from time import sleep
-from base.utils import yell
+from base.utils import yell,
+
 
 class Command(BaseCommand):
     help = 'Sends a special channel message each minute for periodic tasks'
@@ -11,6 +13,6 @@ class Command(BaseCommand):
             while True:
                 yell("base.management.commands.ticktock sending message to ticktock channel")
                 Channel('ticktock').send({'ticktock':1})
-                sleep(60)
+                sleep(60-(now().second))
         except KeyboardInterrupt:
             return
